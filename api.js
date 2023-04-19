4c39d1a853f2c2ff3f582cfc9cf15fcca986212e
dan1aa <burmeidan@gmail.com>
2023-04-19 14:43:59 +0300

    test

---
+++ api.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const multer = require("multer");
+const { Configuration, OpenAIApi } = require("openai");
+
+const router = express.Router();
+const upload = multer();
+
+const configuration = new Configuration({
+    apiKey: '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',
+});
+
+async function transcribe(buffer) {
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createTranscription(
+        buffer, // The audio file to transcribe.
+        "whisper-1", // The model to use for transcription.
+        undefined, // The prompt to use for transcription.
+        'json', // The format of the transcription.
+        1, // Temperature
+        'en' // Language
+    )
+    return response;
+}
+
+router.post("/", upload.any('file'), (req, res) => {
+    audio_file = req.files[0];
+    buffer = audio_file.buffer;
+    buffer.name = audio_file.originalname;
+    const response = transcribe(buffer);
+    response.then((data) => {
+        res.send({
+            type: "POST",
+            transcription: data.data.text,
+            audioFileName: buffer.name
+        });
+    }).catch((err) => {
+        res.send({ type: "POST", message: err });
+    });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-// const WebSocket = require('ws')
+const express = require('express');
+const cors = require('cors');
 
-// const server = new WebSocket.Server({ port: 3000 })
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-// server.on('connection', ws => {
-//     console.log('New client connected')
-//     ws.on('message', message => {
-//         server.clients.forEach(client => {
-//             if(client.readyState === WebSocket.OPEN) {
-//                 client.send(JSON.stringify(message))
-//             }
-//         })
-//     })
-// })
+app.use('/', require('./api'));
 
-const express = require('express')
-
-let app = express()
-
-app.get('/', (req, res) => {
-    res.end('hi')
-})
-
-app.listen(3000, () => {
-    console.log('server')
-})
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Ready to Go!');
+});
